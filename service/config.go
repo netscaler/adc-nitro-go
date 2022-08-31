63e67a3a0f312b0c5ee95160149ffb97b059fe22
christosm <christos.mersinis@citrix.com>
2022-08-31 10:24:19 +0000

    add check for snmptrap_snmpuser_binding
    Signed-off-by: christosm <christos.mersinis@citrix.com>

--- service/config.go
+++ service/config.go
@@ -336,8 +336,15 @@ func (c *NitroClient) DeleteResource(resourceType string, resourceName string) e
 //DeleteResourceWithArgs deletes a resource of supplied type and name. Args are supplied as an array of strings
 //Each array entry is formatted as "key:value"
 func (c *NitroClient) DeleteResourceWithArgs(resourceType string, resourceName string, args []string) error {
+	//Remove unwanted argument (username) for listing but keep it for delete operation
+	var err error
+	if resourceType == "snmptrap_snmpuser_binding" {
+		get_args := args[:len(args)-1]
+		_, err = c.listResourceWithArgs(resourceType, resourceName, get_args)
 
-	_, err := c.listResourceWithArgs(resourceType, resourceName, args)
+	} else {
+		_, err = c.listResourceWithArgs(resourceType, resourceName, args)
+	}
 	if err == nil { // resource exists
 		c.logger.Trace("DeleteResource Found resource ", "resourceType", resourceType, "resourceName", resourceName)
 		_, err = c.deleteResourceWithArgs(resourceType, resourceName, args)
